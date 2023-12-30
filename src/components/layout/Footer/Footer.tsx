3d8a4a9b9aa55048ddd8ee809210c74ca0ba1a36
makorella <makorella2238@gmail.com>
2023-12-30 12:46:09 +0300

    fixed

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -2,8 +2,8 @@ const Footer = () => {
     return (
         <footer className="fixed bg-gray-800 text-white p-4 w-full bottom-0">
             <div className="text-sm sm:text-base container mx-auto">
-                <p className="text-center">&copy; 2023 Kirill G. Все права защищены.</p>
-                <p className="text-center mt-2">Welcome to the Complete Catalog of Cars produced worldwide since 1945</p>
+                <p className="text-sm sm:text-base text-center">&copy; 2023 Kirill G. Все права защищены.</p>
+                <p className="text-sm sm:text-base text-center mt-2">Welcome to the Complete Catalog of Cars produced worldwide since 1945</p>
             </div>
         </footer>
     );
